f75ef9d4ae2866809b70945d3f41af32ee1ee775
Thunwara <mtheewara@gmail.com>
2024-01-27 18:26:14 +0700

    try using sql, failed

---
+++ database/databasepg.js
@@ -0,0 +1,21 @@
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   host: "localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: "TThun2608.",
+//   database: "postgres"
+// });
+
+// client.connect();
+
+// client.query(`SELECT * FROM users`, (err, res) => {
+//     if (!err) {
+//         console.log(res.rows);
+//     } else {
+//         console.error(err.message);
+//     }
+
+//     client.end();
+// });
